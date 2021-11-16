b508326da825d3bbd700c89d9cbf6348fc0e1ea0
Matthew <2565361+mshafer1@users.noreply.github.com>
2021-11-15 21:18:08 -0600

    Update setup_machine.sh

--- setup_machine.sh
+++ setup_machine.sh
@@ -13,5 +13,7 @@ curl "$scripts_root/_install_web_servers.sh" | bash -
 curl "$scripts_root/_install_docker.sh" | bash -
 
 # configure Git aliases
-
 curl "https://gist.githubusercontent.com/mshafer1/c4ad7a6a3b287ec62bc7f5cf568e01ac/raw/3224ad66913be93137dc9729786c95e866fe9fcb/new_machine_git_setup.bash" | bash -
+
+# configure bash aliases
+curl "$scripts_root/_setup-aliases.sh" | bash -
