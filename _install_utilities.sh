13469154095a98749d13efa468ba5070243bd77b
Matthew <2565361+mshafer1@users.noreply.github.com>
2021-11-15 20:33:40 -0600

    Create _install_utilities.sh

---
+++ _install_utilities.sh
@@ -0,0 +1,17 @@
+apt-get update
+
+apt-get upgrade -y
+
+apt-get install \
+  nano \
+  tmux \
+  python3-venv \
+  python3-pip \
+  python3-dev \
+  python3-setuptools \
+  python-is-python3 \
+  build-essential \
+  libssl-dev \
+  libffi-dev \
+  -y
+  
