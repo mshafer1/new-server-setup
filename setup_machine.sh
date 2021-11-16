55a923c1a375fa3169927073365b12816944b35c
Matthew <2565361+mshafer1@users.noreply.github.com>
2021-11-15 21:27:31 -0600

    Update setup_machine.sh
    copy aliases into place

--- setup_machine.sh
+++ setup_machine.sh
@@ -16,4 +16,5 @@ curl "$scripts_root/_install_docker.sh" | bash -
 curl "https://gist.githubusercontent.com/mshafer1/c4ad7a6a3b287ec62bc7f5cf568e01ac/raw/3224ad66913be93137dc9729786c95e866fe9fcb/new_machine_git_setup.bash" | bash -
 
 # configure bash aliases
-curl "https://gist.githubusercontent.com/mshafer1/11c693baf0e0a70a83f208d4bdfc9554/raw/f9b93a5400822e70cf7aee25a7c6be510f32d34a/bash_aliases.sh" | bash -
+curl "https://gist.githubusercontent.com/mshafer1/11c693baf0e0a70a83f208d4bdfc9554/raw/f9b93a5400822e70cf7aee25a7c6be510f32d34a/bash_aliases.sh" >> ~/.bash_aliases
+source .bash_aliases
