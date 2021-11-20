f5389134e3780aeb57d1848e62e52b70a81e8384
Matthew <2565361+mshafer1@users.noreply.github.com>
2021-11-20 09:14:51 -0600

    Update _setup_ssh_key.sh

--- _setup_ssh_key.sh
+++ _setup_ssh_key.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -v
 set -e
 
 key_file="${1:-~/.ssh/id_rsa}"
@@ -10,3 +10,4 @@ fi
 
 echo "Generating ssh key $key_file"
 ssh-keygen -t rsa -N '' -f "$key_file"
+cat $key_file.pub
