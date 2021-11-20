f0c5c8cae57680278d701bf814ee81fa3ac851bf
Matthew <2565361+mshafer1@users.noreply.github.com>
2021-11-20 09:26:26 -0600

    Update _setup_ssh_key.sh
    got it working

--- _setup_ssh_key.sh
+++ _setup_ssh_key.sh
@@ -1,13 +1,12 @@
 #!/bin/bash -v
 set -e
 
-key_file="${1:-~/.ssh/id_rsa}"
+key_file=${1:-~/.ssh/id_rsa}
 
-if [ -f "$key_file" ]; then
+if [[ -f $key_file ]]; then
   echo "File $key_file already exists. Skipping."
   exit 0
 fi
 
 echo "Generating ssh key $key_file"
-ssh-keygen -t rsa -N '' -f "$key_file"
-cat $key_file.pub
+ssh-keygen -t rsa -N '' -f $key_file
