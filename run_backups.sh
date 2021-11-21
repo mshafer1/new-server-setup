c9eb5f3c72dce68d168c4288813f2b02ff3a3346
Matthew <2565361+mshafer1@users.noreply.github.com>
2021-11-21 17:45:29 -0600

    Create run_backups.sh

---
+++ run_backups.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -v
+set -e
+log_file=/var/log/updates.log
+apt-get update >> $log_file
+apt-get upgrady -y >> $log_file
+if test -f /var/run/reboot-required; then
+  reboot >> $log_file
+fi
