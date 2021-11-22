512d64a0fc3679273af8850e695a23b4e21e5da9
Matthew <2565361+mshafer1@users.noreply.github.com>
2021-11-21 18:03:38 -0600

    fix typo

--- run_updates.sh
+++ run_updates.sh
@@ -2,7 +2,7 @@
 set -e
 log_file=/var/log/updates.log
 apt-get update >> $log_file
-apt-get upgrady -y >> $log_file
+apt-get upgrade -y >> $log_file
 if test -f /var/run/reboot-required; then
   reboot >> $log_file
 fi
