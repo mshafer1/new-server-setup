70bce408adf67d93fbaae27bfd2d1144e711d79f
Matthew <2565361+mshafer1@users.noreply.github.com>
2022-02-15 07:33:47 -0600

    explicitly install git first

--- _install_utilities.sh
+++ _install_utilities.sh
@@ -4,22 +4,25 @@ apt-get update
 
 apt-get upgrade -y
 
-apt-get install \
+apt-get install -y \
   nano \
   tmux \
   make \
   wget \
   curl \
   git-all \
-  python3-venv \
-  python3-pip \
-  python3-dev \
-  python3-setuptools \
-  python-is-python3 \
   build-essential \
   libssl-dev \
-  libffi-dev \
-  -y
+  libffi-dev
+
+# python likes git to already be installed
+apt-get install -y \
+  python3-dev \
+  python3-venv \
+  python3-pip \
+  python3-setuptools \
+  python-is-python3
+
  
 # extra Py libraries - https://github.com/pyenv/pyenv/wiki#troubleshooting--faq
 apt-get install \
